41769f68375fa218f39325214bfb4b2e936a8d55
Satyam Thakur <sat.thakur20@gmail.com>
2024-01-10 20:49:30 +0530

    Creating AppModule and Application class for DI

---
+++ app/src/main/java/com/satyamthakur/flashnotes/di/AppModule.kt
@@ -0,0 +1,46 @@
+package com.satyamthakur.flashnotes.di
+
+import android.app.Application
+import androidx.room.Room
+import com.satyamthakur.flashnotes.feature_note.data.data_source.NoteDatabase
+import com.satyamthakur.flashnotes.feature_note.data.repository.NoteRepositoryImplementation
+import com.satyamthakur.flashnotes.feature_note.domain.repository.NoteRepository
+import com.satyamthakur.flashnotes.feature_note.domain.use_case.DeleteNote
+import com.satyamthakur.flashnotes.feature_note.domain.use_case.GetNotes
+import com.satyamthakur.flashnotes.feature_note.domain.use_case.NoteUseCases
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Provides
+    @Singleton
+    fun providesDatabase(app: Application): NoteDatabase {
+        return Room.databaseBuilder(
+            app,
+            NoteDatabase::class.java,
+            NoteDatabase.DATABASE_NAME
+        ).build()
+    }
+
+    @Provides
+    @Singleton
+    fun providesNoteRepository(db: NoteDatabase): NoteRepository {
+        return NoteRepositoryImplementation(db.noteDao)
+    }
+
+    @Provides
+    @Singleton
+    fun providesNoteUseCases(repository: NoteRepository): NoteUseCases {
+        return NoteUseCases(
+            getNotes = GetNotes(repository),
+            deleteNote = DeleteNote(repository)
+        )
+    }
+
+}
