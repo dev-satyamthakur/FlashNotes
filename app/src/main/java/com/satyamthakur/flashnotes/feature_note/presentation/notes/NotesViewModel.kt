d0bcedddb2317cc5f844fe192a9755d20dd358f4
Satyam Thakur <sat.thakur20@gmail.com>
2024-01-11 20:07:58 +0530

    Creating NotesState data class for ViewModel states

---
+++ app/src/main/java/com/satyamthakur/flashnotes/feature_note/presentation/notes/NotesViewModel.kt
@@ -0,0 +1,15 @@
+package com.satyamthakur.flashnotes.feature_note.presentation.notes
+
+import androidx.lifecycle.ViewModel
+import com.satyamthakur.flashnotes.feature_note.domain.use_case.NoteUseCases
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class NotesViewModel @Inject constructor(
+    private val noteUseCases: NoteUseCases
+): ViewModel() {
+
+    
+
+}
